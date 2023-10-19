8b65dc13f83a3cc2b0b3d849ff57f73e224fe65b
Derin DeNio <derindenio@gmail.com>
2023-10-19 14:22:31 -0600

    Added a genre property to the Parent class for all 3 child classes to inherite

--- js/main.js
+++ js/main.js
@@ -3,11 +3,12 @@
 //Going to start with Encapsulation, a base Class for the collection
    //adding getters to add some abstraction 
 class Gamelibrary{
-    constructor(name,system,era,language){
+    constructor(name,system,era,language,genre){
         this._name = name;
         this._system = system;
         this._era = era;
         this._language = language;
+        this._genre = genre;
     }
     get name(){
         return this._name
@@ -21,6 +22,9 @@ class Gamelibrary{
     get language(){
         return this._language
     }
+    get genre(){
+        return this._genre
+    }
     description() {
         console.log(`This game is titled ${this._name}, it was originally made for the ${this._system} system, in the ${era}`)
     }
@@ -29,8 +33,8 @@ class Gamelibrary{
 //Now adding Inheritance; some child classes that will extend our Parent Gamelibrary class
 
 class American extends Gamelibrary{
-    constructor(name,system,era,language,USrating){
-        super(name,system,era,language);
+    constructor(name,system,era,language,genre,USrating){
+        super(name,system,era,language,genre);
         this._USrating = USrating;
         
     }
@@ -39,13 +43,13 @@ class American extends Gamelibrary{
     };
     
     description() {
-        console.log(`This game is called ${this._name}, it was made in the ${this._era} for the ${this._system} and used the ${this._language} language, it had a rating in the US of ${this._USrating}%`)
+        console.log(`This game is called ${this._name}, it was a(n) ${this._genre}-style game made in the ${this._era} for the ${this._system} and used the ${this._language} language, it had a rating in the US of ${this._USrating}%`)
     }
 }
 
 class Japanese extends Gamelibrary{
-    constructor(name,system,era,language,Japaneserating){
-        super(name,system,era,language);
+    constructor(name,system,era,language,genre,Japaneserating){
+        super(name,system,era,language,genre);
         this._Japaneserating = Japaneserating;
         
     }
@@ -54,13 +58,13 @@ class Japanese extends Gamelibrary{
     };
     
     description() {
-        console.log(`This game is called ${this._name}, it was made in the ${this._era} for the ${this._system} and used the ${this._language} language, it had a rating in the US of ${this._Japaneserating}%`)
+        console.log(`This game is called ${this._name}, it was a(n) ${this._genre}-style game made in the ${this._era} for the ${this._system} and used the ${this._language} language, it had a rating in the US of ${this._Japaneserating}%`)
     }
 }
 
 class European extends Gamelibrary{
-    constructor(name,system,era, language,Europeanrating){
-        super(name,system,era,language);
+    constructor(name,system,era, language,genre,Europeanrating){
+        super(name,system,era,language,genre);
         this._Europeanrating = Europeanrating;
         
     }
@@ -69,13 +73,13 @@ class European extends Gamelibrary{
     };
     
     description() {
-        console.log(`This game is called ${this._name}, it was made in the ${this._era} for the ${this._system} and used the ${this._language} language, it had a rating in Europe of ${this._Europeanrating}%`)
+        console.log(`This game is called ${this._name}, it was a(n) ${this._genre}-style game made in the ${this._era} for the ${this._system} and used the ${this._language} language, it had a rating in Europe of ${this._Europeanrating}%`)
     }
 }
 
-let sonic = new American('Sonic The Hedgehog', 'Sega Genesis','90s','English',82);
-let finalfantasy = new Japanese('Final Fantasy', 'Famicon','80s','Japanese',95);
-let earthlight = new European('Earthlight','ZX Spectrum','80s','English',80);
+let sonic = new American('Sonic The Hedgehog', 'Sega Genesis','90s','English', 'platform',82);
+let finalfantasy = new Japanese('Final Fantasy', 'Famicon','80s','Japanese','JRPG',95);
+let earthlight = new European('Earthlight','ZX Spectrum','80s','English','scrolling shooter',80);
 
 //Utilizing Polymorphism here
 let collection = [sonic,finalfantasy,earthlight]
